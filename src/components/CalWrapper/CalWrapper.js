23ef5157371ad9c5542a0bd870285d14a8fd3b70
achalkharabe <achalkharabe1@gmail.com>
2023-05-23 01:17:52 +0530

    commit:cal-app

--- src/components/CalWrapper/CalWrapper.js
+++ src/components/CalWrapper/CalWrapper.js
@@ -1,21 +1,29 @@
 
 
 import Button from "../../common/Button";
+import './Calwrapper.css';
 
 const CalWrapper = (props) => {
 
-    const {allBtArr } = props;
+    const {allBtArr ,oprationArr} = props;
 
     const allButton = allBtArr.map((btObj) => {
-        return    <Button btName={btObj.value} btFun={btObj.btFun} />
+        return    <Button key={btObj.value} btName={btObj.value} btFun={btObj.btFun} />
+    });
+    const alloprationButton= oprationArr.map((btObj) => {
+        return    <Button key={btObj.value} btName={btObj.value} btFun={btObj.btFun} />
     });
 
     console.log('allButton',allButton)
 
     return (
-        <div>
-        
+        <div className="cal">
+        <div className="bt-fun">
         {allButton}
+        </div>
+        <div className="opfun">
+        {alloprationButton}
+        </div>
         </div>
     )
 };
--- src/components/cal/Cal.js
+++ src/components/cal/Cal.js
@@ -1,29 +1,116 @@
 
 import { useState } from "react";
 import CalWrapper from "../CalWrapper/CalWrapper";
-
+import "./cal.css";
 const Cal = () => {
 
 
     const [inputValue , setinputValue]=useState(0);
+    const[operationValue,setOperationValue]=useState(0);
+    const[currentOpration,setCurrentOpration]=useState('');
     const allBtArr = [
         {value: 0, btFun: () => {
-            setinputValue(0)
+            
+            setinputValue(prevValue=>parseInt(prevValue.toString()+0))
         }},
         {value: 1, btFun: () => {
             
-            setinputValue(1)}},
+            setinputValue(prevValue=>parseInt(prevValue.toString()+1))}},
         {value: 2, btFun: () => {
-            setinputValue(2)}},
+            setinputValue(prevValue=>parseInt(prevValue.toString()+2))}},
         {value: 3, btFun: () => {
-            setinputValue(3)
+           
+             setinputValue(prevValue=>parseInt(prevValue.toString()+3))
+        }},
+        {value: 4, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+4))
+        }},
+        
+        {value: 5, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+5))
+        }},
+        {value: 6, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+6))
+        }},
+        {value: 7, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+7))
+        }},
+        {value: 8, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+8))
+        }},
+        {value: 9, btFun: () => {
+            setinputValue(prevValue=>parseInt(prevValue.toString()+9))
         }},
         
     ];
+
+    const oprationArr=[
+       
+        {
+            value: '+',
+            btFun:()=>{
+                setCurrentOpration('+');
+          setOperationValue(inputValue);
+          setinputValue(0);
+            }
+        },
+        {
+            value: '-',
+            btFun:()=>{
+                setCurrentOpration('-');
+          setOperationValue(inputValue);
+          setinputValue(0);
+            }
+        },
+        {
+            value: '*',
+            btFun:()=>{
+                setCurrentOpration('*');
+          setOperationValue(inputValue);
+          setinputValue(0);
+            }
+        },
+        {
+            value: '/',
+            btFun:()=>{
+                setCurrentOpration('/');
+          setOperationValue(inputValue);
+          setinputValue(0);
+            }
+        },
+        
+        {
+           value:'=',
+           btFun:()=>{
+            if(currentOpration==='+'){
+
+                setinputValue((prevValue)=>{
+                    return prevValue+operationValue;
+                })
+            }else if(currentOpration==='-'){
+                setinputValue((prevValue)=>{
+                    return operationValue-prevValue;
+                })
+            }
+            else if(currentOpration==='*'){
+                setinputValue((prevValue)=>{
+                    return prevValue* operationValue;
+                })
+            } 
+            else if(currentOpration==='/'){
+                setinputValue((prevValue)=>{
+                    return operationValue/prevValue ;
+                })
+            } 
+           }
+        }
+    ];
+
     return (
-        <div>
+        <div className="p">
             <p> This is cal js {inputValue}</p>
-            <CalWrapper  allBtArr={allBtArr}/>
+            <p>{operationValue}{currentOpration}</p>
+            <CalWrapper  allBtArr={allBtArr} oprationArr={oprationArr}/>
         </div>
     )
 };
